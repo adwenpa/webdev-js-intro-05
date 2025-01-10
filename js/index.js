2835871415444b13a2bb02e9247adae61adecc83
adwenpa <joojo.abr@gmail.com>
2025-01-10 17:19:11 -0500

    read index.html and experimented inside index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+// 
+// const colors = ["red", "yellow", "blue"];
+// colors[6] = "green";
+// console.log(colors);
+// const items = colors.keys();
+// console.log(items);
+// for (const key of items) {
+//   console.log(`${key}: ${colors[key]}`);
+// }
+
+// const plants = ["broccoli", "cauliflower", "cabbage", "kale", "tomato"];
+// console.log(plants);
+// console.log(plants.pop());
+// console.log(plants.pop());
+// console.log(plants.pop()); 
